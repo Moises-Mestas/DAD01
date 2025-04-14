0378bc6ac270ca7bf91b67571bb58dea55448a6d
Moises-Mestas <moisesjoaquin51@gmail.com>
2025-04-13 22:08:09 -0500

    xd

--- ms_pedido/src/main/java/com/example/mspedido/MsPedidoApplication.java
+++ ms_pedido/src/main/java/com/example/mspedido/MsPedidoApplication.java
@@ -17,13 +17,5 @@ public class MsPedidoApplication {
         SpringApplication.run(MsPedidoApplication.class, args);
     }
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(new Info()
-                .title("OPEN API MICROSERVICIO PEDIDO")
-                .version("0.0.1")
-                .description("servicio web pedido")
-                .termsOfService("http://swagger.io/terms")
-                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
-    }
+
 }
